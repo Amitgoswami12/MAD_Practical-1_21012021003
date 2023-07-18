5388c4045085747741dde9d0979fa37714714c9e
student <student@gnu.ac.in>
2023-07-18 08:22:27 +0530

    Created practical_1.3

---
+++ src/main/kotlin/Practical-1.3.kt
@@ -0,0 +1,30 @@
+fun main(){
+    println("Enter enrollment number");
+    var a = readLine();
+
+    println("Enter your name: ");
+    var b = readLine();
+
+    println("Enter your class: ");
+    var c = readLine();
+
+    println("Enter your branch: ");
+    var d = readLine();
+
+    println("Enter your college name: ");
+    var e = readLine();
+
+    println("Enter your university name: ");
+    var f = readLine();
+
+
+    println("..........Student information............");
+
+    println("Enrollment No.: $a");
+    println("Name: $b");
+    println("Class: $c");
+    println("Branch: $d");
+    println("College Name: $e");
+    println("University Name: $f");
+
+}
