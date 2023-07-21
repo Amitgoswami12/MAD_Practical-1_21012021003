b40e3b9c7489bed402144a080d238f2304d034b0
Amit <amitgoswami698@gmail.com>
2023-07-21 23:14:40 +0530

    done

---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    println("21012021003_AMIT GOSWAMI\n")
+
+    val a1 : Int = 25
+    val a2=a1.toDouble()
+    println("Double value(from integer) = $a2")
+
+    val b1 : String = "45568"
+    val b2=b1.toInt()
+    println("Integer value(from String) = $b2")
+
+    val c1 : String = "4568"
+    val c2 = c1.toDouble()
+    println("Double value(from String) = $c2")
+}
